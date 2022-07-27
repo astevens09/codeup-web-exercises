fd2a6ee897fbae8cc71a36f2bac92b1df7ada06f
Anthony Stevens <aastevens1126@gmail.com>
2022-07-27 16:29:47 -0500

    JavaScript with HTML Exercises

---
+++ JavaScriptWithHtml/js/external.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("Hello from external JavaScript");
+
+alert("Welcome to my Website!");
+var userInput = prompt("What is your favorite color?");
+alert("Great, "+userInput+" is my favorite color too!");
