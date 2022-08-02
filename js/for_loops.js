fff2c887c8913da7eacb357065f41aaa0c16b521
Anthony Stevens <aastevens1126@gmail.com>
2022-08-02 12:05:17 -0500

    for_loop exercise 4 complete

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,33 +1,49 @@
-//This function logs the mulitiplication table for the passed in value
-function showMultiplicationTable(num){
-  for(var i=1; i<=10; i++){
-    console.log(
-      num+" x "+i+" = "+ num*i);
-  };
-}
+// //This function logs the mulitiplication table for the passed in value
+// function showMultiplicationTable(num){
+//   for(var i=1; i<=10; i++){
+//     console.log(
+//       num+" x "+i+" = "+ num*i);
+//   };
+// }
+//
+// //Below is an encapsulated function that prints 10 random number between 20-200 to the console
+// (function(){
+//   var num = 0;
+//
+//   for(var i=0; i<10;i++){
+//    num = randomNumber();
+//
+//    console.log(isEven(num));
+//   }
+//
+//   //This function returns whether the passed in value is even or odd
+//   function isEven(num){
+//
+//    if(num%2==0){
+//     return num +" is even";
+//    }else{
+//     return num +" is odd";
+//    }
+//   }
+//
+//   //This function returns a random number between 20 and 200
+//   function randomNumber(){
+//    return Math.floor(Math.random()*181)+20;
+//   }
+// })()
 
-//Below is an encapsulated function that prints 10 random number between 20-200 to the console
+//This function prints the index the same number of times as the index
 (function(){
-  var num = 0;
+  for(var i=1; i < 10; i++){
+ var num = i;
+ var numS = num.toString();
+ numString= num.toString();
 
-  for(var i=0; i<10;i++){
-   num = randomNumber();
+  for(var j=1; j<i; j++){
+   numString+= numS;
+ }
 
-   console.log(isEven(num));
-  }
+ console.log(Number(numString));
 
-  //This function returns whether the passed in value is even or odd
-  function isEven(num){
-
-   if(num%2==0){
-    return num +" is even";
-   }else{
-    return num +" is odd";
-   }
-  }
-
-  //This function returns a random number between 20 and 200
-  function randomNumber(){
-   return Math.floor(Math.random()*181)+20;
-  }
+}
 })()
