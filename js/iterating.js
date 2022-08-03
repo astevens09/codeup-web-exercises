7c9cfc258e14b98b8cc66fc3f4dca41910134518
Anthony Stevens <aastevens1126@gmail.com>
2022-08-03 11:37:44 -0500

    Updated while.js and created iterating html and Js files

---
+++ js/iterating.js
@@ -0,0 +1,67 @@
+(function() {
+  "use strict";
+  /**
+   * TODO:
+   * Create an array of 4 people's names and store it in a variable called
+   * 'names'.
+   */
+  let names = ['Tom', 'Jane', 'Frank', 'Pope'];
+  /**
+   * TODO:
+   * Create a log statement that will log the number of elements in the names
+   * array.
+   */
+  console.log(`names array length is: ${names.length}`);
+
+  /**
+   * TODO:
+   * Create log statements that will print each of the names individually by
+   * accessing each element's index.
+   */
+  names.forEach(name => {
+    console.log(name);
+  });
+  /**
+   * TODO:
+   * Write some code that uses a for loop to log every item in the names
+   * array.
+   */
+  for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+  }
+  /**
+   * TODO:
+   * Refactor your above code to use a `forEach` loop
+   */
+  names.forEach(name => {
+    console.log(name);
+  });
+  /**
+   * TODO:
+   * Create the following three functions, each will accept an array and
+   * return an an element from it
+   * - first: returns the first item in the array
+   * - second: returns the second item in the array
+   * - last: returns the last item in the array
+   *
+   * Example:
+   *  > first([1, 2, 3, 4, 5]) // returns 1
+   *  > second([1, 2, 3, 4, 5]) // returns 2
+   *  > last([1, 2, 3, 4, 5]) // return 5
+   */
+
+  function first(list) {
+    return list[0];
+  }
+
+  function second(list) {
+    return list[1];
+  }
+
+  function third(list) {
+    return list[2];
+  }
+
+  
+
+})();
--- js/while.js
+++ js/while.js
@@ -4,3 +4,32 @@ while(j<65536){
   console.log(j);
   j*=2;
 }
+
+
+//Ice cream Exercises
+
+var conesToSell = Math.floor(Math.random()*50+50);
+var soldCones = 0;
+
+//Function returns random value between 1 and 5
+var randomCones = ()=>{return Math.floor(Math.random()*5)+1};
+var purchasedCones = 0;
+var conesLeft = 0;
+
+do{
+   purchasedCones = randomCones();
+   soldCones +=purchasedCones;
+   conesLeft = conesToSell-soldCones;
+
+
+   if(soldCones<conesToSell){
+       console.log(`${soldCones} cones sold.`);
+   }else if(purchasedCones > conesLeft){
+       console.log(`Cannot sell you ${purchasedCones} cones I only have ${conesLeft}`);
+   }
+
+
+
+}while(soldCones< conesToSell);
+
+console.log('Yay! I sold them all!');
